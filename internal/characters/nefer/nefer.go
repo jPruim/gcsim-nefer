6771d5ddbefa1dc008c6a16216e2032bd21f6157
jace <jace@Kali-3.attlocal.net>
2025-10-24 06:47:55 -0400

    Nefer: Basic Data

---
+++ internal/characters/nefer/nefer.go
@@ -0,0 +1,45 @@
+package nefer
+
+import (
+	tmpl "github.com/genshinsim/gcsim/internal/template/character"
+	"github.com/genshinsim/gcsim/pkg/core"
+	"github.com/genshinsim/gcsim/pkg/core/info"
+	"github.com/genshinsim/gcsim/pkg/core/keys"
+	"github.com/genshinsim/gcsim/pkg/core/player/character"
+)
+
+func init() {
+	core.RegisterCharFunc(keys.Nefer, NewChar)
+}
+
+type char struct {
+	*tmpl.Character
+}
+
+func NewChar(s *core.Core, w *character.CharWrapper, _ info.CharacterProfile) error {
+	c := char{}
+	c.Character = tmpl.NewWithWrapper(s, w)
+
+	c.EnergyMax = 60
+	c.NormalHitNum = normalHitNum
+	c.SkillCon = 3
+	c.BurstCon = 5
+
+	w.Character = &c
+
+	return nil
+}
+
+func (c *char) Init() error {
+	return nil
+}
+
+func (c *char) AnimationStartDelay(k info.AnimationDelayKey) int {
+	if k == info.AnimationXingqiuN0StartDelay {
+		return 11
+	}
+	if k == info.AnimationYelanN0StartDelay {
+		return 11
+	}
+	return c.Character.AnimationStartDelay(k)
+}
